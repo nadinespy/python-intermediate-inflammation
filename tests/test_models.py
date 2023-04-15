041bd612b7b81921ff04cf9caf5bdb647ebe3ac0
nadinespy <nadine.spychala@gmail.com>
2023-04-15 09:44:36 +0100

    Add parameterisation mean, min, max test cases

--- tests/test_models.py
+++ tests/test_models.py
@@ -5,6 +5,17 @@ import numpy.testing as npt
 import pytest
 
 
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([ [0, 0], [0, 0], [0, 0] ], [0, 0]),
+        ([ [1, 2], [3, 4], [5, 6] ], [3, 4]),
+    ])
+def test_daily_mean(test, expected):
+    """Test mean function works for array of zeroes and positive integers."""
+    from inflammation.models import daily_mean
+    npt.assert_array_equal(daily_mean(np.array(test)), np.array(expected))
+
 def test_daily_mean_zeros():
     """Test that mean function works for an array of zeros."""
     from inflammation.models import daily_mean
@@ -31,6 +42,17 @@ def test_daily_mean_integers():
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
 
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([ [0, 0], [0, 0], [0, 0] ], [0, 0]),
+        ([ [1, 2], [3, 4], [5, 6] ], [5, 6]),
+    ])
+def test_daily_max(test, expected):
+    """Test mean function works for array of zeroes and positive integers."""
+    from inflammation.models import daily_max
+    npt.assert_array_equal(daily_max(np.array(test)), np.array(expected))
+
 def test_daily_max_integers():
     """Test that max function works for an array of positive integers."""
     from inflammation.models import daily_max
@@ -44,7 +66,19 @@ def test_daily_max_integers():
     npt.assert_array_equal(daily_max(test_input), test_result)
 
 
-def test_daily_max_integers():
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([ [0, 0], [0, 0], [0, 0] ], [0, 0]),
+        ([ [1, 2], [3, 4], [5, 6] ], [1, 2]),
+    ])
+def test_daily_min(test, expected):
+    """Test mean function works for array of zeroes and positive integers."""
+    from inflammation.models import daily_min
+    npt.assert_array_equal(daily_min(np.array(test)), np.array(expected))
+
+
+def test_daily_min_integers():
     """Test that min function works for an array of positive integers."""
     from inflammation.models import daily_min
 
