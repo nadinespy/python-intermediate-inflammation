8b22bb5b754f0e335d6b5e84e2a77e4947177349
nadinespy <nadine.spychala@gmail.com>
2023-04-15 13:43:47 +0100

    modified normalizing function, including testing

--- inflammation/models.py
+++ inflammation/models.py
@@ -20,15 +20,23 @@ def load_csv(filename):
 
 def patient_normalise(data):
     """
-    Normalise patient data from a 2D inflammation data array.
-    NaN values are ignored, and normalised to 0.
-    Negative values are rounded to 0.
+    Normalise patient data between 0 and 1 of a 2D inflammation data array.
+
+    Any NaN values are ignored, and normalised to 0
+
+    :param data: 2D array of inflammation data
+    :type data: ndarray
+
     """
     if np.any(data < 0):
-        raise ValueError('Inflammation values should not be negative')
-    max = np.nanmax(data, axis=1)
+        raise ValueError('inflammation values should not be negative')
+    if not isinstance(data, np.ndarray):
+        raise TypeError('data input should be ndarray')
+    if len(data.shape) != 2:
+        raise ValueError('inflammation array should be two-dimensional')
+    max_val = np.nanmax(data, axis=1)
     with np.errstate(invalid='ignore', divide='ignore'):
-        normalised = data / max[:, np.newaxis]
+        normalised = data / max_val[:, np.newaxis]
     normalised[np.isnan(normalised)] = 0
     normalised[normalised < 0] = 0
     return normalised
